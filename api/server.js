2c963c656e33edfc2ecd22cd4624f9020d8b8538
sxwvn <sxwvnart@proton.me>
2025-10-27 08:37:48 +0700

    fix: fucking cors

--- api/server.js
+++ api/server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 
 dotenv.config()
-app.use(cors())
 
 // Create an Express application
 const app = new Express()
 app.use(Express.json())
+app.use(cors())
 
 // Attach data router, include 'data' before the endpoint name
 app.use('/api', DataRouter)
